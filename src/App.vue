89724a375d0fefc1810bf46f0dcc9eee5ab96288
sangwoo <lesw1216@gmail.com>
2024-01-30 03:58:41 +0900

    feat: add header and navigator

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import NavigatorComp from './components/NavigatorComp.vue';
 </script>
 
 <template>
+  <div>
+    <header>
+      <h1>Query Maker</h1>
+    </header>
+    <nav>
+      <NavigatorComp></NavigatorComp>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/NavigatorComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/schema">create schema</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/table">table create</router-link>
+        </li>
+      </ul>
+    </div>
+  </template>
+  <script setup>
+  
+  </script>
+  <style lang="">
+      
+  </style>
