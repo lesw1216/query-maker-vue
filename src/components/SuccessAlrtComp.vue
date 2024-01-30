57d5715cf0afc592b55fd8bee3c96207268c287e
sangwoo <lesw1216@gmail.com>
2024-01-30 23:59:25 +0900

    feat: add success alrt Component

---
+++ src/components/SuccessAlrtComp.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class="alert alert-secondary mb-2 d-flex" v-if="isSuccess">
+                <div class="w-100 align-self-center">
+                    {{message}}
+                </div>
+                <Button class="flex-shrink-1" :button-type="'btn-outline-secondary btn-sm'" :button-name="'copy'"
+                        @click="copy(message)"></Button>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    isSuccess: Boolean,
+    message: String
+})
+
+import { useClipboard } from '@vueuse/core'
+import { ref } from 'vue';
+
+import Button from './ButtonComp.vue';
+
+const queryMessage=ref(props.message)
+const {copy} = useClipboard({ queryMessage })
+</script>
+<style lang="">
+    
+</style>
