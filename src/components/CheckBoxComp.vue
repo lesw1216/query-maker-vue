dd5c6bc073d309fec9f7c2503705709792efc209
sangwoo <lesw1216@gmail.com>
2024-01-31 17:32:07 +0900

    feat: add check box component

---
+++ src/components/CheckBoxComp.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="isCheck" @click="$emit('clickCheck', isCheck)" v-model="isCheck">
+        <label class="form-check-label" for="isCheck">
+            {{checkName}}
+        </label>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    checkName: String
+})
+
+import { ref } from 'vue';
+
+const isCheck = ref(false)
+
+</script>
+<style lang="">
+    
+</style>
