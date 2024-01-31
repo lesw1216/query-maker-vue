e5a865098843740a3037ca7b7e25f61f88253494
sangwoo <lesw1216@gmail.com>
2024-01-31 17:36:12 +0900

    feat: add current nav bar display feat

--- src/components/NavigatorComp.vue
+++ src/components/NavigatorComp.vue
@@ -2,20 +2,33 @@
     <div>
       <ul class="nav nav-tabs">
         <li class="nav-item">
-          <router-link class="nav-link" to="/schema">create schema</router-link>
+          <router-link class="nav-link" :class="{active: isPath('/schema')}" to="/schema">create schema</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/table">table create</router-link>
+          <router-link class="nav-link" :class="{active: isPath('/table')}" to="/table">table create</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/select">select</router-link>
+          <router-link class="nav-link" :class="{active: isPath('/select')}" to="/select">select</router-link>
         </li>
       </ul>
     </div>
-  </template>
-  <script setup>
-  
-  </script>
-  <style lang="">
-      
-  </style>
+</template>
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const route = useRoute()
+const router = useRouter()
+
+const routePath = ref()
+
+router.afterEach(() => {
+  routePath.value = route.path
+})
+
+const isPath = (path) => {
+  return routePath.value == path
+}
+</script>
+<style lang="">
+</style>
