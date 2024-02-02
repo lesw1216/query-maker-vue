68382e7ffc281de7be73126b5d9b015a9ffe372a
sangwoo <lesw1216@gmail.com>
2024-02-03 04:31:40 +0900

    refactor: schema 쿼리 생성 리팩토링

--- src/views/SchemaView.vue
+++ src/views/SchemaView.vue
@@ -1,48 +1,45 @@
 <template lang="">
     <div>
         <div class="m-2 d-flex flex-column">
-            <SuccessAlrt :message="schemaQuery" :isSuccess="isSchemaQuery"></SuccessAlrt>
-            <InputComp :input-label="'SchemaName'" :is-valid="isValidCheck(validErrorMessages.schemaName)" @request-button="createForm"></InputComp>
-            <div class='alert alert-danger mt-2' v-if="isValidCheck(validErrorMessages.schemaName)">
-                {{validErrorMessages.schemaName}}
+            <InputComp class='mb-2' :input-label="'SchemaName'" :is-valid="isValid(responseError.schemaName)" @request-button="createForm"></InputComp>
+            <SuccessAlrt :message="responseSuccess.createdQuery" :isSuccess="responseSuccess.isSuccess"></SuccessAlrt>
+            <div class='alert alert-danger' v-if="!isValid(responseError.schemaName)">
+                {{responseError.schemaName}}
             </div>
         </div>
     </div>
 </template>
 <script setup>
-import { ref, reactive }  from "vue"
-import { useClipboard } from '@vueuse/core'
+// vue
+import { ref, reactive, inject }  from "vue"
 
 import axios from '@/api/axios'
 
+// components
 import SuccessAlrt from "@/components/SuccessAlrtComp.vue";
 import Button from '@/components/ButtonComp.vue';
 import InputComp from "@/components/InputComp.vue";
 
+// global
+const { isValid, responseDataToField, objectInit } = inject('$globalMethods')
+
 // ref
-const schemaQuery = ref("")
-const isSchemaQuery = ref(false)
 
 // reactive
-const validErrorMessages = reactive({
-    schemaName:undefined
+const responseSuccess = reactive({
+    createdQuery:null,
+    isSuccess:false
 })
 
+const responseError = reactive({
+})
+
+
 // function
-const isValidCheck = (field) => {
-    return field != undefined
-}
-
-const initCondition = () => {
-    isSchemaQuery.value = false
-    validErrorMessages.schemaName=undefined;
-}
-
-// vueUse
-const {copy} = useClipboard({ schemaQuery })
 
 const createForm = (schemaNameProps) => {
-    initCondition()
+    objectInit(responseError)
+    objectInit(responseSuccess)
 
     axios.post('/schemas', {
         schemaName: schemaNameProps
@@ -51,20 +48,11 @@ const createForm = (schemaNameProps) => {
         console.log(response.data);
 
         if(response.data['code'] == 200 && response.data['httpStatus'] == 'OK') {
-            schemaQuery.value = response.data['data'];
-            isSchemaQuery.value = true;
+            responseSuccess.createdQuery = response.data['data'];
+            responseSuccess.isSuccess = true;
         }
     }).catch(function(error) {
-        const responseData = error.response['data']['data'];
-        const keys = Object.keys(validErrorMessages)
-        
-        // key is index
-        for (const key in keys) {
-            const keyName = keys[key]
-            validErrorMessages[keyName]=responseData[keyName]
-        }
-
-        // validErrorMessages.schemaName=error.response['data']['data'][validErrorMessages.schemaName]
+        responseDataToField(error.response['data']['data'], responseError)
     });
 }
 </script>
