735a512eff7335588fca334da5ea9a2c9016fc8b
sangwoo <lesw1216@gmail.com>
2024-02-02 14:31:53 +0900

    feat: 검증을 위한 조건 추가

--- src/components/InputComp.vue
+++ src/components/InputComp.vue
@@ -6,13 +6,15 @@
             </div>
             <Button :button-name="'CREATE'" @click="$emit('requestButton', inputValue)" :button-type="'btn-primary'"></Button>
         </div>
-        <input class="form-control" type="text" aria-label="input label" 
+        <input class="form-control" :class="[inputClass, isValid ? 'border-danger' : '']" type="text" aria-label="input label" 
                     v-model="inputValue">
     </div>
 </template>
 <script setup>
 const props = defineProps({
-    inputLabel: String,
+    isValid: Boolean,
+    inputClass: String,
+    inputLabel: String
 })
 
 import { ref } from 'vue';
