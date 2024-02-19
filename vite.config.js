78618253e8a8981c0c56c5f5d0231a4129bde50b
sangwoo <lesw1216@gmail.com>
2024-02-19 21:08:17 +0900

    Chore: 배포 개발 환경 분리 설정

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,48 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   server: {
+//   }
+// })
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      // 개발 서버 설정
+      server: {
+        proxy: {
+          '/api': 'http://localhost:8080'
+        }
+      }
     }
-  },
-  server: {
+  } else {
+    // command === 'build'
+    return {
+      // 빌드 설정
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      }
+    }
   }
 })
