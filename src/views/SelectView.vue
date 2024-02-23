ffb8435fd8a6b3dd7492dc14f41b53c2d47929b2
Sangwoo Lee <lesw1216@gmail.com>
2024-02-23 20:13:18 +0900

    refactor: select 페이지 디자인 변경 (#5)
    부트스트랩에서 테일윈드로 변경

--- src/views/SelectView.vue
+++ src/views/SelectView.vue
@@ -1,43 +1,54 @@
 <template lang="">
     <div>
-        <div class="d-flex flex-column m-2">
+        <div class="flex flex-col m-2">
             <InputComp class="mb-2" :input-label="'Table Name'" :is-valid="isValid(responseError.tableName)" @request-button="requestDefualtSelect"></InputComp>
             <SuccessAlrtComp :is-success="responseSuccess.isSuccess" :message="responseSuccess.createdQuery"></SuccessAlrtComp>
-            <div class='alert alert-danger mt-2' v-if="!isValid(responseError.tableName)">
+            <div class='valid-error' v-if="!isValid(responseError.tableName)">
                 {{responseError.tableName}}
             </div>
-            <hr class="border-4">
-            <div class="d-flex justify-content-between">
+            <div class="flex justify-between items-center mt-2">
                 <div>
                     Columns
                 </div>
-                <div class="d-flex">
-                    <CheckBoxComp class="me-2 align-self-center" :check-name="'ALL SELECT'" @click-check="onclickIsAllSelect"></CheckBoxComp>
-                    <div class="d-flex" v-if="!isAllSelect">
-                        <ButtonComp class="me-2" :button-name="'Add'" :button-type="'btn-secondary'" @click="onclickIncreaseRow">
+                <div class="flex">
+                    <CheckBoxComp class="me-2 self-center" :check-name="'ALL SELECT'" @click-check="onclickIsAllSelect"></CheckBoxComp>
+                    <div class="flex" v-if="!isAllSelect">
+                        <ButtonComp class="mr-2" :button-name="'Add'" :button-type="'btn-default'" @click="onclickIncreaseRow">
                         </ButtonComp>
-                        <ButtonComp :button-name="'ALL DEL'" :button-type="'btn-danger'" @click="onclickAllDeleteColumnName">
+                        <ButtonComp :button-name="'ALL DEL'" :button-type="'btn-default btn-delete'" @click="onclickAllDeleteColumnName">
                         </ButtonComp>
                     </div>
                     
                 </div>
             </div>
-            <div class='alert alert-danger mt-2' v-if="isGlobalValid">
+            <div class='valid-error' v-if="isGlobalValid">
                 Must Input ColumnName
             </div>
-            <div class="d-flex justify-content-between mt-2" v-for="(columnName, index) in columnNames" v-if="!isAllSelect">
-                <div class="d-flex flex-column me-2 flex-grow-1">
+            <div class="grid grid-cols-3 text-center">
+                <div>Column Name</div>
+                <div>As Name</div>
+                <div></div>
+            </div>
+            <div class="grid grid-cols-3 gap-2" v-for="(columnName, index ) in columnNames" v-if="!isAllSelect">
+                <input type="text" class="border-2 focus:outline-none focus:border-blue-500 p-1" id="columnInput" v-model="columnName.name"
+                    :class="[isValid(responseError['columnNames['+index+'].name']) ? '':'border-danger']">
+                <input type="text" class="border-2 focus:outline-none focus:border-blue-500 p-1" id="asInput" v-model="columnName.asName">
+                <ButtonComp class="" :button-name="'DEL'" :button-type="'btn-default btn-delete w-full'" 
+                    @click="onclickDeleteColumnName(index)"></ButtonComp>
+            </div>
+            <!-- <div class="mt-2" v-for="(columnName, index) in columnNames" v-if="!isAllSelect">
+                <div class="me-2 w-1/3 border">
                     <label for="columnInput" class="form-label">Column Name</label>
                     <input type="text" class="form-control" id="columnInput" v-model="columnName.name"
                     :class="[isValid(responseError['columnNames['+index+'].name']) ? '':'border-danger']">
                 </div>
-                <div class="d-flex flex-column flex-grow-1">
+                <div class="border">
                     <label for="asInput" class="form-label">As Name</label>
                     <input type="text" class="form-control" id="asInput" v-model="columnName.asName">
                 </div>
                 <ButtonComp class="align-self-end ms-2" :button-name="'DEL'" :button-type="'btn-danger'" 
                     @click="onclickDeleteColumnName(index)"></ButtonComp>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -133,6 +144,14 @@ const onclickIncreaseRow = () => {
 }
 
 const onclickDeleteColumnName = (index) => {
+    if (index == 0) {
+        columnNames.value[0] = {
+            name:null,
+            asName:null
+        }
+
+        return;
+    }
     console.log("onclickDeleteColumnName")
     columnNames.value.splice(index, 1)
 }
